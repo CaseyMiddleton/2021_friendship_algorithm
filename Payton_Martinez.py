eff419f8756dcc706750905b712b677671b728c9
Payton711 <pama8418@colorado.edu>
2021-08-17 16:28:55 -0600

    Final

--- Payton_Martinez.py
+++ Payton_Martinez.py
@@ -10,15 +10,61 @@ def play_game():
 	while user_entry != 1 and user_entry != 2: user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
 	# if the user selects 1, they want to play! Ask them questions and wait for their answers.
 	while user_entry == 1:
-		## STEP 1 HERE
-		
-		## STEP 2&3 HERE
-	
-		## STEP 4 HERE
-	
-		
-		user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
-		user_entry = int(user_entry)
+		total_points = 0
+
+		## QUESTION 1
+		answer_one = input('Select Option!\n1.I have a pet.\n2. I do not have a pet.\n\nYour Selection: ')
+		if answer_one == 1:
+			total_points=+5
+			answer_one_b = input('Select Option!\n1.I have a dog.\n2. I have another type of pet.\n\nYour Selection: ')
+			if answer_one_b == 1:
+				total_points=+5
+			else
+				break
+		else
+			answer_one_c = input('Select Option!\n1.I like dogs\n2. I do not like dogs.\n\nYour Selection: ')
+			if answer_one_b == 1:
+				total_points=+3
+			else
+				break
+
+		## QUESTION 2
+		answer_two = input('Select Option!\n1.I like sports.\n2. I do not like sports.\n\nYour Selection: ')
+		if answer_two == 1:
+			total_points = +5
+			answer_two_b = input('Select Option!\n1.I like to play sports.\n2. I only like to watch sports.\n\nYour Selection: ')
+			if answer_two_b == 1:
+				total_points = +5
+			else
+				break
+		else
+			answer_two_c = input('Select Option!\n1.I tolerate sports\n2. I hate sports.\n\nYour Selection: ')
+			if answer_two_c == 1:
+				total_points = +3
+			else
+				break
+		## QUESTION 3
+		answer_two = input('Select Option!\n1.I like to play video games.\n2. I do not like to play video games.\n\nYour Selection: ')
+		if answer_two == 1:
+			total_points = +5
+			answer_two_b = input('Select Option!\n1.I like to play computer games.\n2. I like to game on other consoles.\n\nYour Selection: ')
+			if answer_two_b == 1:
+				total_points = +5
+			else
+				break
+		else
+			answer_two_c = input('Select Option!\n1.I like to play other type of games\n2. I hate any type of games.\n\nYour Selection: ')
+			if answer_two_c == 1:
+				total_points = +3
+			else
+				break
+
+		if total_points >= 20
+			print("Great Friends!")
+		elif total_points >= 15
+			print("Friends!")
+		else
+			print("Maybe we can be friends later...")
 	
 
 
